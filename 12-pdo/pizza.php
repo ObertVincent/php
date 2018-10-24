0d156cff58477e57a1b5daa3e59f7a1f858f7ba4
OBERT Vincent <obert.vincent@laposte.net>
2018-10-24 12:57:23 +0200

    Création du dossier projet example

--- 12-pdo/pizza.php
+++ 12-pdo/pizza.php
@@ -7,13 +7,23 @@ $db = new PDO('mysql:host=localhost;dbname=pizzastore;charset=utf8','root','',[P
 
 $query = $db -> query('select * from pizza where id= 3');
 $pizza= $query->fetch();
-var_dump($pizza);
+echo $pizza ['id'].':'.$pizza['name'];
 
 //2.Récupérer l'id dynamiquement à partir de l'url 
 // Exemple si je saisis pizza.php?id=7, je dois récupérer la pizza avec l'id 7
 
-$id = $_GET['id'];
+$id = isset($_GET['id']) ? $_GET['id'] : 0;
 
+//if (is_numeric($id)){
 $query = $db->query('SELECT name FROM pizza WHERE id ='.$id);
 $pizza = $query->fetch();
-var_dump($pizza);
+//var_dump($pizza);
+
+if ($pizza) {
+    echo $pizza['id'].':'.$pizza['name'];
+}else { 
+    echo '<br>La pizza n\'existe pas';
+}
+//}else {
+//   echo 'la Pizza n\'existe pas'
+//}
---
+++ 13-pizzastore/README.md
@@ -0,0 +1,18 @@
+# Pizza Store PDO SQL
+
+ - Récupérer un backup de la BDD pizzastore
+L'intérêt est de pouvoir recréer la structure de la base à tout moment.
+
+Au niveau du PHP, on va créer quelques fichiers / dossier :
+
+- config/database.php -> Connection à la base de données en PDO, sera inclus dans tous les fichiers PHP
+- partials/header.php -> le header du site à inclure dans toutes les pages (Bootstrap CDN)
+- partials/footer.php -> le footer du site à inclure dans toutes les pages
+- index.php -> la page d'accueil du site
+- pizza_list.php -> Lister toutes les pizzas de la base de données
+- pizza_single.php -> la page d'une pizza seule
+
+
+Au niveau du front.
+
+- assets/ -> contiendra le css, le js, les images
